0a92984aecd90952276507229c743f1b66b011a6
Burt Beckwith <burt@burtbeckwith.com>
2013-08-15 18:21:33 -0400

    if you use jars in the lib directory, it's much harder for applications to update dependencies, and in this case these jars should override the dependencies from the spring-social-core plugin but they can't as jars, only as Maven-style dependencies in the same scope

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,12 +9,22 @@ grails.project.dependency.resolution = {
         grailsCentral()
         mavenLocal()
         mavenCentral()
+        mavenRepo 'http://maven.springframework.org/milestone/org/springframework/social/spring-social-core/1.1.0.M3/'
     }
 
     dependencies {
         compile "javax.inject:javax.inject:1"
         compile "org.codehaus.jackson:jackson-mapper-asl:1.9.9"
-		  compile("org.springframework.security:spring-security-crypto:3.1.0.RC3") { transitive = false }
+        compile 'org.springframework.security:spring-security-crypto:3.1.0.RC3', {
+            excludes 'commons-logging', 'spring-core'
+        }
+
+        compile 'org.springframework.social:spring-social-core:1.1.0.M3', {
+            excludes 'httpclient', 'spring-jdbc', 'spring-security-crypto', 'spring-web'
+        }
+        compile 'org.springframework.social:spring-social-web:1.1.0.M3', {
+            excludes 'javax.servlet.jsp-api', 'servlet-api', 'spring-social-core', 'spring-web', 'spring-webmvc'
+        }
     }
 
     plugins {
