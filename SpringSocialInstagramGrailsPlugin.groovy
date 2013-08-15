ae23ad6ffba09722cef3aa20b963b969d6c3b841
Michael Astreiko <michael.astreiko@gmail.com>
2013-08-16 00:19:59 +0300

    Clean up

--- SpringSocialInstagramGrailsPlugin.groovy
+++ SpringSocialInstagramGrailsPlugin.groovy
@@ -9,65 +9,36 @@ class SpringSocialInstagramGrailsPlugin {
     ]
 
     Map dependsOn = ['springSocialCore': '0.1.31 > *']
-    // TODO Fill in these fields
     def title = "Spring Social Instagram Plugin" // Headline display name of the plugin
     def author = "Michael Astreiko"
     def authorEmail = "michael.astreiko@realizeideas.net"
     def description = '''\
 Grails plugin to use Instagram via Spring Social API.
 '''
-    def scm = [ url: "https://github.com/RealizeIdeas/grails-spring-social-instagram" ]
-    def license = "APACHE"
-    def issueManagement = [ system: "GitHub", url: "https://github.com/RealizeIdeas/grails-spring-social-instagram/issues" ]
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/spring-social-instagram"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [ name: "REID Consulting", url: "http://realizeideas.net/" ]
 
     // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def developers = [ [ name: "Michael Astreiko", email: "michael.astreiko@realizeideas.net" ],
+                        [ name: "Vladimir Moroz", email: "moroz@realizeideas.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GitHub", url: "https://github.com/RealizeIdeas/grails-spring-social-instagram/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/RealizeIdeas/grails-spring-social-instagram" ]
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
 
     def doWithSpring = {
         xmlns context: "http://www.springframework.org/schema/context"
         context.'component-scan'('base-package': "net.realizeideas.springconfig.instagram")
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
     }
 }
